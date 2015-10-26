9f9adca5f73be9d83530af6f8377597dc716e271
Sovada <harmony.dev.info@gmail.com>
2015-10-26 12:23:27 +0100

    single blog done

--- app.js
+++ app.js
@@ -84,7 +84,23 @@ app.get("/blog", function (req, res) {
     } else {
         res.render("blog/index", {"translation": translation.en});
     }
-})
+});
+
+app.get("/blog/:blogID", function (req, res, next) {
+    currentLanguage = req.headers["accept-language"][0] + req.headers["accept-language"][1];
+    // recup le parametre portfolioID de mon URL
+    blogID = req.params.blogID;
+
+    if (currentLanguage === "en") {
+        res.render("blog/single", {"translation": translation.en, "id": blogID});
+    } else if (currentLanguage === "fr") {
+        res.render("blog/single", {"translation": translation.fr, "id": blogID});
+    } else if (currentLanguage === "es") {
+        res.render("blog/single", {"translation": translation.es, "id": blogID});
+    } else {
+        res.render("blog/single", {"translation": translation.en, "id": blogID});
+    }
+});
 
 app.listen(port);
 console.log(`Server's running at ${env}${port}`);
